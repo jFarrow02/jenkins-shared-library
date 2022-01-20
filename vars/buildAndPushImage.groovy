7979dfa73fd8776a8da2f55507a15a4c3c99042f
jFarrow02 <jack.dempsey.farrow@gmail.com>
2022-01-20 14:38:36 -0500

    adds env vars for nexus repo

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: 'nexus-my-docker-hostedrepo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
+        sh "docker build -t $IMAGE ."
+        sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS"
+        sh 'docker push $IMAGE'
     }
 }
