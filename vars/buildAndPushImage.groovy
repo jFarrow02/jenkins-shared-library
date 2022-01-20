62c894e59a3527cb4d46aeadfe8c9b594ae843c7
jFarrow02 <jack.dempsey.farrow@gmail.com>
2022-01-20 15:20:35 -0500

    adds imageName and repoLocation as function parameters

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(string imageName, string repoLocation) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-my-docker-hostedrepo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh "docker build -t $IMAGE ."
-        sh "echo $PWD | docker login -u $USER --password-stdin $NEXUS"
+        sh "docker build -t $imageName ."
+        sh "echo $PWD | docker login -u $USER --password-stdin $repoLocation"
         sh 'docker push $IMAGE'
     }
 }
